7a3a707d235ea1b6f5b655307571782bd68571f8
paologaleotti <paologaleotti2002@gmail.com>
2026-04-17 09:59:43 +0200

    feat: add ascii art in index and refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+// @ts-check
 import cloudflare from "@astrojs/cloudflare"
 import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
-// @ts-check
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
--- src/content.config.ts
+++ src/content.config.ts
@@ -7,7 +7,7 @@ const events = defineCollection({
     schema: z.object({
         date: z.string(),
         name: z.string(),
-        status: z.string(),
+        status: z.enum(["OPEN", "CLOSED", "SOON"]),
         slots: z.string(),
         games: z.string(),
         image: z.string().optional(),
